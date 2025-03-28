7a45d7abb76ffd00c2a83f40b372fec478f0db7b
ouattara <ouattaraibrahimyacouba@gmail.com>
2025-03-28 16:26:28 +0000

    adaptation du env

--- todo_app_backend/db/db.connect.js
+++ todo_app_backend/db/db.connect.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // Chargement des variables d'environnement
 
 const URI_PRODUCTION = process.env.MONGO_URI
 // importation de l'url de base de données
 
-
 const connect_bd = async () => {
+    if (!URI_PRODUCTION) {
+        console.error("MONGODB_URI n'est pas définie .env");
+        process.exit(1); // Exit if URI is missing
+    }
     try {
         await mongoose.connect(URI_PRODUCTION);
         console.log("Connexion à la base de données réussie");

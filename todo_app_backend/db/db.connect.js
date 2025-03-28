4344fc2aa07b0df54e4b4c7eeafbbe0efba6a3ff
ouattara <ouattaraibrahimyacouba@gmail.com>
2025-03-28 12:57:36 +0000

    mise à jour de l'uri mongodb

--- todo_app_backend/db/db.connect.js
+++ todo_app_backend/db/db.connect.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const URI_PRODUCTION = process.env.MONGO_URI
 // importation de l'url de base de données
-const URI = process.env.NODE_ENV !== 'production' ? "mongodb://localhost:27017/bridge" : URI_PRODUCTION
+
 
 const connect_bd = async () => {
     try {
-        await mongoose.connect(URI);
+        await mongoose.connect(URI_PRODUCTION);
         console.log("Connexion à la base de données réussie");
     } catch (error) {
         console.error("Echec de connexion à la base de donnée:", error.message);

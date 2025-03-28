0ddd12402c4c518bae2932e857a212370c42e377
ouattara <ouattaraibrahimyacouba@gmail.com>
2025-03-28 04:02:46 +0000

    ajout des variables d'environnement

--- todo_app/src/api/api.ts
+++ todo_app/src/api/api.ts
@@ -1,4 +1,8 @@
-const BASE_URL = "https://test-bridge-todo-app.onrender.com";
+const URL_BACKEND_PROD = import.meta.env.VITE_BACKEND_PROD_URL;
+const URL_BACKEND_DEV = import.meta.env.VITE_BACKEND_DEV_URL;
+const ENV = import.meta.env.VITE_NODE_ENV;
+
+const BASE_URL = ENV === "production" ? URL_BACKEND_PROD : URL_BACKEND_DEV;
 
 export const API_ADD_TODO = `${BASE_URL}/api/todo/add-todo`;
 export const API_GETALL_TODO = `${BASE_URL}/api/todo/get-todo`;

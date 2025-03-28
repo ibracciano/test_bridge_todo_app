62562ee3e3eca73ad459b05a18d0f2e9d881d0be
ouattara <ouattaraibrahimyacouba@gmail.com>
2025-03-28 00:25:28 +0000

    modification1 frontend

--- todo_app/src/api/api.ts
+++ todo_app/src/api/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://test-bridge-todo-app.onrender.com";
 
 export const API_ADD_TODO = `${BASE_URL}/api/todo/add-todo`;
 export const API_GETALL_TODO = `${BASE_URL}/api/todo/get-todo`;
